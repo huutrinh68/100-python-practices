7639c5787ea73b8d9bf8a90c43d8b32fd750da8c
huutrinh68 <trinhsp89@gmail.com>
2017-11-10 23:39:01 +0900

    add 00~04.py

--- Chapter01/00.py
+++ Chapter01/00.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #-*- codding: utf-8 -*-
 # 00.py
+# Slice pratice
 
 str = "stressed"
-print(str[-1::-1])
+print(str[-1::-1])	#desserts
+print(str[::-3])	#dst
+print(str[-3::-2])	#set
